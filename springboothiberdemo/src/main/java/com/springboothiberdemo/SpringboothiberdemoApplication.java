928652c0f59bcfe9ba51af2bb8f695217119f86a
saraniya.k <saraniya.k@LP-5CD9024KND.HCLT.CORP.HCL.IN>
2019-10-22 12:03:36 +0530

    message

---
+++ springboothiberdemo/src/main/java/com/springboothiberdemo/SpringboothiberdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboothiberdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothiberdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboothiberdemoApplication.class, args);
+	}
+
+}
---
+++ springboothiberdemo/src/test/java/com/springboothiberdemo/SpringboothiberdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboothiberdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringboothiberdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
